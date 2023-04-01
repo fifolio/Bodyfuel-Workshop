d5312524b7074d16036a39428b314dc58826e7c3
Firas <firasdabbabi@gmail.com>
2023-04-01 16:30:24 +0100

    version 1.1: Files & Folders Structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import "./App.css";
+import {Route, Routes} from 'react-router-dom';
+import {Box} from "@mui/material";
+
+// import components
+import Navbar from "./components/Navbar";
+import Footer from "./components/footer"
+import Home from "./pages/Home"
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
     return (
-    <div className="App">
-      Bodyfuel App
-    </div>
+      <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
+      <Navbar />
+      
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/exercise/:id" element={<ExerciseDetail />}></Route>
+        </Routes>
+
+      <Footer />
+      </Box>
   )
 }
 
---
+++ src/pages/ExerciseDetail.tsx
@@ -0,0 +1,7 @@
+export default function ExerciseDetail(){
+    return (
+        <>
+        <h1>ExerciseDetails</h1>
+        </>
+    )
+}
