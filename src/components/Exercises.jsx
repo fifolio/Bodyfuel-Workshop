6f73f97a38f90e47041f6adb6cebcab29fe8d995
Firas <firasdabbabi@gmail.com>
2023-04-09 17:49:25 +0100

    version 2.1: Add Pagination to Exercises.jsx & add scroll feature to Search Bar

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,25 +1,47 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
-import {Box, Stack, Typography} from '@mui/material';
-import {exercisesOptions, fetchData} from '../utils/fetchData.js'
+import { Box, Stack, Typography } from '@mui/material';
+import { exercisesOptions, fetchData } from '../utils/fetchData.js'
 import ExerciseCard from './ExerciseCard';
 
-export default function Exercises ({exercises, setExercises, bodyPart}){
-  console.log(exercises)
-  return (
-    <Box 
-    id="exercises"
-    sx={{mt: {lg: '110px'}}}
-    mt="50px"
-    p="20px"
-    >
-    <Typography variant="h4" mb="46px">Showing Results</Typography>  
-    <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
-      {exercises.map((exercise, index) => (
-        <ExerciseCard key={index} exercise={exercise} />
-      ))}
+export default function Exercises({ exercises, setExercises, bodyPart }) {
 
-    </Stack>
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 3000, behavior: 'smooth' })
+  }
+
+
+  return (
+    <Box
+      id="exercises"
+      sx={{ mt: { lg: '110px' } }}
+      mt="50px"
+      p="20px"
+    >
+      <Typography variant="h4" mb="46px">Showing Results</Typography>
+      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+
+      </Stack>
     </Box>
   )
 }
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,13 +1,13 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exercisesOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-export default function SearchExercises ({setExercises, bodyPart, setBodyPart}){
+export default function SearchExercises({ setExercises, bodyPart, setBodyPart }) {
 
   const [search, setSearch] = useState('')
   const [bodyParts, setBodyParts] = useState([])
- 
+
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exercisesOptions);
@@ -19,58 +19,60 @@ export default function SearchExercises ({setExercises, bodyPart, setBodyPart}){
   }, []);
 
   const handleSearch = async () => {
-    if (search){
+    if (search) {
       const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exercisesOptions);
 
       const searchedExercise = exercisesData.filter(
         (item) => item.name.toLowerCase().includes(search)
-               || item.target.toLowerCase().includes(search)
-               || item.equipment.toLowerCase().includes(search)
-               || item.bodyPart.toLowerCase().includes(search),
+          || item.target.toLowerCase().includes(search)
+          || item.equipment.toLowerCase().includes(search)
+          || item.bodyPart.toLowerCase().includes(search),
       );
 
       setSearch('');
       setExercises(searchedExercise)
+      window.scrollTo({ top: 3000, behavior: 'smooth' })
     }
   }
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '30px'}}} mb="50px" textAlign="center">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="50px" textAlign="center">
         Awesome Exercises You <br /> Should Discover
       </Typography>
 
       <Box position="relative" mb="72px">
-        <TextField sx={{ input: {fontWeight: '700', border: 'none',borderRadius: '4px'},
-          width: {lg: '800px', xs: '350px'},
+        <TextField sx={{
+          input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+          width: { lg: '800px', xs: '350px' },
           height: '76px',
           backgroundColor: '#fff',
           borderRadius: '40px'
         }}
-        value={search}
-        onChange={(e) => setSearch(e.target.value.toLowerCase())}
-        placeholder='Search Exercise'
-        type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder='Search Exercise'
+          type="text"
         />
 
-       <Button
-        sx={{
-          backgroundColor: "#FF2625",
-          color: 'white',
-          fontWeight: 'bold',
-          textTransform: 'none',
-          width: {lg: '175px', xs: '80px'},
-          fontSize: {lg: '20px', xs: '14px'},
-          height: '56px',
-          position: 'absolute',
-          right: '0'
-        }}
-        variant="contained" 
-        color="error"
-        onClick={handleSearch}
+        <Button
+          sx={{
+            backgroundColor: "#FF2625",
+            color: 'white',
+            fontWeight: 'bold',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0'
+          }}
+          variant="contained"
+          color="error"
+          onClick={handleSearch}
         >
-          Search
-        </Button> 
+          Discover
+        </Button>
 
       </Box>
 
