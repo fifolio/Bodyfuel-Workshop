777e6b3ef28492c5af7beeaee2f8e863dd023aa4
Firas <firasdabbabi@gmail.com>
2023-04-10 17:55:19 +0100

    version 2.6: Added youtubeOptions to fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,16 @@ export const exercisesOptions = {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
+ 
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f3827e2852msh4875279ea61b21fp184088jsnfa5c512a3452',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+  
 
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
